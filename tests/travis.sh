c01f8b89d11504da0cb456c213153add4428b9df
Cuong Tien Dinh <deptteam.cuong@gmail.com>
2021-01-23 11:50:28 +0700

    Build trigger

--- tests/travis.sh
+++ tests/travis.sh
@@ -14,6 +14,11 @@ done
 DATA_DIR="$(pwd)/test_data"
 PLUGINS_DIR="$DATA_DIR/plugins"
 
+dateAndMonth=`date`
+BUILDPHPV=$(php -r 'echo PHP_VERSION;')
+NBPHPV="7.3.25"
+OLDBLD=$(expr $TRAVIS_BUILD_NUMBER - 1)
+
 rm -rf "$DATA_DIR"
 rm UNWDS.phar 2> /dev/null
 mkdir "$DATA_DIR"
@@ -50,4 +55,30 @@ elif [ $(grep -c "ERROR\|CRITICAL\|EMERGENCY" "$DATA_DIR/server.log") -ne 0 ]; t
 	exit 1
 else
 	echo All tests passed
+fi
+
+if [ "$BUILDPHPV" = "$NBPHPV" ]; then
+    echo "PHP $BUILDPHPV detected. Ignore the phar push and then exit..."
+else
+    echo "PHP $BUILDPHPV detected. Pushing the phar into output repo..."
+	git config --global user.name "Cuong Tien Dinh"
+    git config --global user.email "deptteam.cuong@gmail.com"
+	chmod 777 UNWDS.phar
+    git clone https://github.com/dtcu0ng/UNWDS_Output.git
+	cd UNWDS_Output
+	git checkout master
+	cd ci_build_output
+	mkdir alpha/old/$OLDBLD
+	cp latest/UNWDS.phar alpha/old/$OLDBLD
+    cd ../../
+	cp UNWDS.phar UNWDS_Output/ci_build_output/alpha/latest
+	cd UNWDS_Output
+	git add -A
+	git commit -m "Alpha build update: $dateAndMonth (Build $TRAVIS_BUILD_NUMBER)"
+	git remote rm origin
+  # Add new "origin" with access token in the git URL for authentication
+    git remote add origin https://dtcu0ng:$GHTOKEN@github.com/dtcu0ng/UNWDS_Output.git > /dev/null 2>&1
+	git pull origin master --rebase
+    git push origin master --quiet
+	echo Push completed with 0 or more errors
 fi
